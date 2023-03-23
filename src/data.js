7942592c00fc784941199ffc5e7b527aa34c0e7a
김준식 <wnstlr8879@naver.com>
2023-03-24 03:37:30 +0900

    상품 가격 변경

--- src/data.js
+++ src/data.js
@@ -3,21 +3,21 @@ let data = [
     id: 0,
     title: "White and Black",
     content: "Born in France",
-    price: 120000,
+    price: 79000,
   },
 
   {
     id: 1,
     title: "Red Knit",
     content: "Born in Seoul",
-    price: 110000,
+    price: 55000,
   },
 
   {
     id: 2,
     title: "Grey Yordan",
     content: "Born in the States",
-    price: 130000,
+    price: 99000,
   },
 ];
 
